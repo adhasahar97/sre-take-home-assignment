abc2e793d75ef6729dab46ce389e37f380462b6e
adhasahar97 <adhasahar97@gmail.com>
2025-06-07 00:50:56 +0800

    Try installing argocd

--- infra/terraform/homelab/main.tf
+++ infra/terraform/homelab/main.tf
@@ -4,9 +4,10 @@ terraform {
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+    config_context = "microk8s"
   }
 }
 
 module "helm" {
-  source                       = "../modules/helm"
+  source = "../modules/helm"
 }
--- infra/terraform/modules/helm/main.tf
+++ infra/terraform/modules/helm/main.tf
@@ -1,6 +1,30 @@
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-    config_context = "microk8s"
+locals {
+  list_of_releases = {
+    "argocd" = {
+      name       = "argocd"
+      repository = "https://argoproj.github.io/argo-helm"
+      chart      = "argo-cd"
+      namespace  = "argocd"
+      version    = "8.0.12"
+      values     = {
+        
+      }
+    },
   }
+}
+
+resource "helm_release" "release" {
+  for_each = local.list_of_releases
+
+  name             = each.value.name
+  repository       = each.value.repository
+  chart            = each.value.chart
+  version          = each.value.version
+  namespace        = each.value.namespace
+  create_namespace = lookup(each.value, "create_namespace", true)
+
+  values = try(
+    [templatefile("${path.module}/values/values-${each.value.name}.yaml", each.value.values)],
+    []
+  )
 }
