5a0f4d85f23dd4cfb419d2df63620cfbe667727f
adhasahar97 <adhasahar97@gmail.com>
2025-06-06 19:00:16 +0800

    Replace hardcoded cidr value with variable

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -44,7 +44,7 @@ resource "aws_route_table" "feedme-sre-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc_cidr_block
     gateway_id = aws_internet_gateway.gw.id
   }
 
