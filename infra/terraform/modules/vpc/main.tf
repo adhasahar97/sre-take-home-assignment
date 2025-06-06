a7d845c053adacdc67e4c3056f5a269d06f1a4bf
adhasahar97 <adhasahar97@gmail.com>
2025-06-06 19:03:41 +0800

    Should bing igw to 0.0.0.0/0 instead

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -44,10 +44,9 @@ resource "aws_route_table" "feedme-sre-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.vpc_cidr_block
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
 
   tags = {
     Name = "rt-feedme-sre"
