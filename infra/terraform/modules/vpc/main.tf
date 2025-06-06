a99d6333e9311d55e766c8a943310604fa0175cc
adhasahar97 <adhasahar97@gmail.com>
2025-06-06 18:52:26 +0800

    Add 3 subnets

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -5,8 +5,29 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr_block, 8, 1)
+  availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "subnet_1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr_block, 8, 2)
+  availability_zone = "ap-southeast-1b"
+
+  tags = {
+    Name = "subnet_2"
+  }
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr_block, 8, 3)
+  availability_zone = "ap-southeast-1c"
+
+  tags = {
+    Name = "subnet_3"
   }
 }
