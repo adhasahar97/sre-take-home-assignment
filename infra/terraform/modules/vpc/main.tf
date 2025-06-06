b70ccdb57f9f19f4b1b3aa20a221e7b9d72c4ba9
adhasahar97 <adhasahar97@gmail.com>
2025-06-06 18:46:33 +0800

    Add subnet

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -1,3 +1,12 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr_block, 8, 0)
+
+  tags = {
+    Name = "subnet_1"
+  }
+}
