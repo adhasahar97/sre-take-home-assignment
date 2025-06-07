4a4ab7e2e90d725320423dba9b06a528321ffe94
adhasahar97 <adhasahar97@gmail.com>
2025-06-07 13:26:16 +0800

    Disable outputs associated to the deleted resouces

--- infra/terraform/modules/cloudflare/outputs.tf
+++ infra/terraform/modules/cloudflare/outputs.tf
@@ -1,9 +1,9 @@
-output "cloudflare_tunnel_token" {
-  value = cloudflare_zero_trust_tunnel_cloudflared.k8s-tunnel.tunnel_token
-}
-output "cloudflare_tunnel_account_id" {
-  value = cloudflare_zero_trust_tunnel_cloudflared.k8s-tunnel.account_id
-}
+# output "cloudflare_tunnel_token" {
+#   value = cloudflare_zero_trust_tunnel_cloudflared.k8s-tunnel.tunnel_token
+# }
+# output "cloudflare_tunnel_account_id" {
+#   value = cloudflare_zero_trust_tunnel_cloudflared.k8s-tunnel.account_id
+# }
 output "cloudflare_tunnel_name" {
   value = var.cloudflare_tunnel_name
 }
