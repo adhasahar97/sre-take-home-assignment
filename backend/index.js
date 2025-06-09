14c31a819df6b4d4f3f978a379a919124444ff65
adhasahar97 <adhasahar97@gmail.com>
2025-06-09 11:56:14 +0800

    Update deployment version

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,38 @@ const client = new MongoClient(url);
 
 const globalOrder = [];
 
+const promBundle = require("express-prom-bundle");
+
+const metricsMiddleware = promBundle({
+  includeMethod: true, 
+  includePath: true, 
+  includeStatusCode: true, 
+  includeUp: true,
+  customLabels: {project_name: 'feedme-sre', project_type: 'test_metrics_labels'},
+  promClient: {
+    collectDefaultMetrics: {
+    }
+  }
+});
+
+app.use(metricsMiddleware)
+
+var winston = require('winston'),
+  expressWinston = require('express-winston');
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
+
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
@@ -29,15 +61,18 @@ async function main() {
 
   app.get("/orders", async (req, res) => {
     const orders = await orderCollection.find({}).toArray();
+    console.log("Get orders", orders)
     res.send(orders);
   });
 
   app.delete("/orders/:id", async (req, res) => {
+    console.log("Delete order", req.params.id);
     await orderCollection.deleteOne({ _id: Number(req.params.id) });
     res.send("ok");
   });
 
   app.post("/orders", async (req, res) => {
+    console.log("Create order");
     const orderSeq = await counterCollection.findOne({ _id: "orderSeq" });
     await orderCollection.insertOne({
       _id: orderSeq.seq++,
