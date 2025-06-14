31fcb97bd564f35add8fcb337047637ed69ccbb4
adhasahar97 <adhasahar97@gmail.com>
2025-06-14 23:44:16 +0800

    Switch to cluster-1

--- infra/terraform/homelab/backend.tf
+++ infra/terraform/homelab/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "adha-homelab-iac-terraform-state"
-    key    = "terraform/feedme-sre-homelab"
+    key    = "terraform/feedme-sre-homelab-cluster-1"
     region = "ap-southeast-1"
   }
 }
