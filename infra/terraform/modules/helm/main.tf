87366d87e3643b7a512576e87eebc05a88f6d661
adhasahar97 <adhasahar97@gmail.com>
2025-06-15 11:56:19 +0800

    Refactor helm releases. Convert to individual resources instead

--- infra/terraform/modules/helm/main.tf
+++ infra/terraform/modules/helm/main.tf
@@ -1,96 +1,84 @@
-locals {
-  list_of_releases = {
-    "cloudflare-tunnel" = {
-      name       = "cloudflare-tunnel"
-      repository = "https://helm.strrl.dev"
-      chart      = "cloudflare-tunnel-ingress-controller"
-      namespace  = "cloudflare-tunnel"
-      version    = "0.0.18"
-      values     = {
-        "cloudflare_tunnel_token"      = var.cloudflare_api_token
-        "cloudflare_tunnel_account_id" = var.cloudflare_tunnel_account_id
-        "cloudflare_tunnel_name"       = var.cloudflare_tunnel_name
-        "cloudflare_domain"            = var.cloudflare_domain
-        "argocd_admin_password"        = var.argocd_admin_password
-      }
-    },
-    "ingress-nginx" = {
-      name       = "ingress-nginx"
-      repository = "https://kubernetes.github.io/ingress-nginx"
-      chart      = "ingress-nginx"
-      namespace  = "ingress-nginx"
-      version    = "4.12.3"
-      values     = {
-        
-      }
-    },
-    "kube-prometheus-stack" = {
-      name       = "kube-prometheus-stack"
-      repository = "https://prometheus-community.github.io/helm-charts"
-      chart      = "kube-prometheus-stack"
-      namespace  = "observability"
-      version    = "73.2.0"
-      values     = {
-        
-      }
-    },
-    "loki" = {
-      name       = "loki"
-      repository = "https://grafana.github.io/helm-charts"
-      chart      = "loki"
-      namespace  = "observability"
-      version    = "6.30.1"
-      values     = {
-        
-      }
-    },
-    "alloy" = {
-      name       = "alloy"
-      repository = "https://grafana.github.io/helm-charts"
-      chart      = "alloy"
-      namespace  = "observability"
-      version    = "1.1.1"
-      values     = {
-        
-      }
-    },
-    # "argocd" = {
-    # name       = "argocd"
-    # repository = "https://argoproj.github.io/argo-helm"
-    # chart      = "argo-cd"
-    # namespace  = "argocd"
-    # version    = "8.0.17"
-    # values     = {
-    # "hostname" = var.cloudflare_domain
-    #   }
-    # }
-  }
+resource "helm_release" "cloudflare-tunnel" {
+  name       = "cloudflare-tunnel"
+  repository = "https://helm.strrl.dev"
+  chart      = "cloudflare-tunnel-ingress-controller"
+  namespace  = "cloudflare-tunnel"
+  version    = "0.0.18"
+  values     = [
+    templatefile("${path.module}/values/values-cloudflare-tunnel.yaml", {
+    "cloudflare_tunnel_token"      = var.cloudflare_api_token
+    "cloudflare_tunnel_account_id" = var.cloudflare_tunnel_account_id
+    "cloudflare_tunnel_name"       = var.cloudflare_tunnel_name
+    "cloudflare_domain"            = var.cloudflare_domain
+    "argocd_admin_password"        = var.argocd_admin_password
+    })
+  ]
+  create_namespace = true
 }
 
-# Trigger update test
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  version    = "4.12.3"
+  values     = [
+    templatefile("${path.module}/values/values-ingress-nginx.yaml", {
+    })
+  ]
+  create_namespace = true
+}
 
-resource "helm_release" "release" {
-  for_each = local.list_of_releases
+resource "helm_release" "kube-prometheus-stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "observability"
+  version    = "73.2.0"
+  values     = [
+    templatefile("${path.module}/values/values-kube-prometheus-stack.yaml", {
+      hostname = var.cloudflare_domain
+    })
+  ]
+  create_namespace = true
+}
 
-  name             = each.value.name
-  repository       = each.value.repository
-  chart            = each.value.chart
-  version          = each.value.version
-  namespace        = each.value.namespace
-  create_namespace = lookup(each.value, "create_namespace", true)
-  # set {
-  # name  = "templates_hash"
-  # value = try(
-  #     sha1(filesha1("${path.module}/values/values-${each.value.name}.yaml")),
-  #     ""
-  #   )
-  # }
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki"
+  namespace  = "observability"
+  version    = "6.30.1"
+  values     = [
+    templatefile("${path.module}/values/values-loki.yaml", {
+    })
+  ]
+  create_namespace = true
+}
 
-  values = try(
-    [templatefile("${path.module}/values/values-${each.value.name}.yaml", each.value.values)],
-    []
-  )
-
+resource "helm_release" "alloy" {
+  name       = "alloy"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "alloy"
+  namespace  = "observability"
+  version    = "1.1.1"
+  values     = [
+    templatefile("${path.module}/values/values-alloy.yaml", {
+    })
+  ]
+  create_namespace = true
+}
+resource "helm_release" "tempo" {
+  name       = "tempo"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "tempo-distributed"
+  namespace  = "observability"
+  version    = "1.42.0"
+  values     = [
+    templatefile("${path.module}/values/values-tempo.yaml", {
+    })
+  ]
+  create_namespace = true
 }
 
 resource "helm_release" "argocd" {
