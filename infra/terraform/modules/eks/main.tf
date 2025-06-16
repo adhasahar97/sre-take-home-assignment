cbad7be39f452b9cd158504791f3f51765e253ce
adhasahar97 <adhasahar97@gmail.com>
2025-06-16 11:40:09 +0800

    Enable EKS auto mode

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -8,6 +8,26 @@ resource "aws_eks_cluster" "feedme-sre" {
   role_arn = aws_iam_role.cluster.arn
   version  = "1.33"
 
+  bootstrap_self_managed_addons = false
+
+  compute_config {
+    enabled       = true
+    node_pools    = ["general-purpose"]
+    node_role_arn = aws_iam_role.node.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
+
   vpc_config {
     subnet_ids = [
       var.subnet_a_id,
@@ -35,48 +55,6 @@ resource "aws_iam_openid_connect_provider" "feedme-sre" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.feedme-sre.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.feedme-sre.identity[0].oidc[0].issuer
-}
-
-data "aws_iam_policy_document" "feedme_sre_ebs_csi_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.feedme-sre.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-    }
-
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.feedme-sre.arn]
-      type        = "Federated"
-    }
-  }
-}
-
-resource "aws_iam_role" "ebs-csi" {
-  assume_role_policy = data.aws_iam_policy_document.feedme_sre_ebs_csi_assume_role_policy.json
-  name               = "AmazonEKS_EBS_CSI_DriverRole"
-}
-
-resource "aws_iam_role_policy_attachment" "ebs-csi" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  role       = aws_iam_role.ebs-csi.name
-}
-
-resource "aws_eks_addon" "ebs-csi-driver" {
-  cluster_name                = aws_eks_cluster.feedme-sre.name
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.44.0-eksbuild.1"
-  resolve_conflicts_on_update = "PRESERVE"
-}
-
-resource "aws_eks_addon" "eks-pod-identity-agent" {
-  cluster_name                = aws_eks_cluster.feedme-sre.name
-  addon_name                  = "eks-pod-identity-agent"
-  addon_version               = "v1.3.7-eksbuild.2"
-  resolve_conflicts_on_update = "PRESERVE"
 }
 
 resource "aws_iam_role" "cluster" {
--- infra/terraform/modules/helm/main.tf
+++ infra/terraform/modules/helm/main.tf
@@ -1,20 +1,20 @@
-resource "helm_release" "cloudflare-tunnel" {
-  name       = "cloudflare-tunnel"
-  repository = "https://helm.strrl.dev"
-  chart      = "cloudflare-tunnel-ingress-controller"
-  namespace  = "cloudflare-tunnel"
-  version    = "0.0.18"
-  values     = [
-    templatefile("${path.module}/values/values-cloudflare-tunnel.yaml", {
-    "cloudflare_tunnel_token"      = var.cloudflare_api_token
-    "cloudflare_tunnel_account_id" = var.cloudflare_tunnel_account_id
-    "cloudflare_tunnel_name"       = var.cloudflare_tunnel_name
-    "cloudflare_domain"            = var.cloudflare_domain
-    "argocd_admin_password"        = var.argocd_admin_password
-    })
-  ]
-  create_namespace = true
-}
+# resource "helm_release" "cloudflare-tunnel" {
+#   name       = "cloudflare-tunnel"
+#   repository = "https://helm.strrl.dev"
+#   chart      = "cloudflare-tunnel-ingress-controller"
+#   namespace  = "cloudflare-tunnel"
+#   version    = "0.0.18"
+#   values     = [
+#     templatefile("${path.module}/values/values-cloudflare-tunnel.yaml", {
+#     "cloudflare_tunnel_token"      = var.cloudflare_api_token
+#     "cloudflare_tunnel_account_id" = var.cloudflare_tunnel_account_id
+#     "cloudflare_tunnel_name"       = var.cloudflare_tunnel_name
+#     "cloudflare_domain"            = var.cloudflare_domain
+#     "argocd_admin_password"        = var.argocd_admin_password
+#     })
+#   ]
+#   create_namespace = true
+# }
 
 resource "helm_release" "ingress-nginx" {
   name       = "ingress-nginx"
@@ -100,37 +100,37 @@ resource "helm_release" "argocd" {
   depends_on = [ kubernetes_ingress_v1.cf-to-nginx ]
 }
 
-resource "kubernetes_ingress_v1" "cf-to-nginx" {
-  metadata {
-    name = "cf-to-nginx"
-    namespace = helm_release.ingress-nginx.namespace
-    annotations = {
-      "cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol" = "http"
-      "cloudflare-tunnel-ingress-controller.strrl.dev/proxy-ssl-verify" = "off"
-    }
-  }
-  spec {
-    ingress_class_name = "cloudflare-tunnel"
-    rule {
-      host = "*.adhshr.xyz"
-      http {
-        path {
-          path = "/"
-          backend {
-            service {
-              name = "ingress-nginx-controller"
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+# resource "kubernetes_ingress_v1" "cf-to-nginx" {
+#   metadata {
+#     name = "cf-to-nginx"
+#     namespace = helm_release.ingress-nginx.namespace
+#     annotations = {
+#       "cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol" = "http"
+#       "cloudflare-tunnel-ingress-controller.strrl.dev/proxy-ssl-verify" = "off"
+#     }
+#   }
+#   spec {
+#     ingress_class_name = "cloudflare-tunnel"
+#     rule {
+#       host = "*.adhshr.xyz"
+#       http {
+#         path {
+#           path = "/"
+#           backend {
+#             service {
+#               name = "ingress-nginx-controller"
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
 
-  depends_on = [ helm_release.cloudflare-tunnel, helm_release.ingress-nginx]
-}
+#   depends_on = [ helm_release.cloudflare-tunnel, helm_release.ingress-nginx]
+# }
 resource "kubernetes_storage_class_v1" "gp2-default" {
   metadata {
     name = "gp2-default"
